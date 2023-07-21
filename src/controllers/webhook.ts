5e5fe20f726c8f7cd21df1fd7a99eebda820da5c
Prince Bondzie <bondzieprince@gmail.com>
2023-07-21 16:30:29 +0000

    update webhook

--- src/controllers/webhook.ts
+++ src/controllers/webhook.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import User, { IUser } from "../models/User";
+import User, { IUser } from "../models/user";
 import Conversation from "../models/conversation";
 import BotResponse from "../models/bot.response";
 import { Configs } from "../config/config";
